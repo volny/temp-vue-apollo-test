6e20552a859fd2d209427c684779627f55c46fc4
Felix Volny <github@volny.co>
2018-10-05 21:05:32 +0200

    use ApolloExample component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ApolloExample />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ApolloExample from './components/ApolloExample.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    ApolloExample
   }
 }
 </script>
